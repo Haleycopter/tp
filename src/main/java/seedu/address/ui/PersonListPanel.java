d5a38528b7732d3690f5d819663d556e8145fda5
CH144 <177772377+CH144@users.noreply.github.com>
2026-03-20 02:33:37 +0000

    Reimplement displaying of contacts using GridPane

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -20,13 +20,14 @@ public class PersonListPanel extends UiPart<Region> {
 
     private static final String FXML = "PersonListPanel.fxml";
 
-    private static final int ENTRY_HEIGHT = 400;
+    private static final int ENTRY_HEIGHT = 270;
     private static final int MIN_ENTRY_WIDTH = 340;
     private static final int SCROLL_BAR_WIDTH = 15;
 
     private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
     private final ObservableList<Person> personList;
     private int numColumns;
+    private int entryWidth;
 
     @FXML
     private ScrollPane scrollPane;
@@ -43,17 +44,47 @@ public class PersonListPanel extends UiPart<Region> {
         super(FXML);
         this.personList = personList;
         this.numColumns = 1;
+        this.entryWidth = MIN_ENTRY_WIDTH;
 
         int usableWidth = (int) primaryStageWidth - SCROLL_BAR_WIDTH;
-        updateEntryWidth(usableWidth);
+        updateNumColumnsAndEntryWidth(usableWidth);
         updateDisplay();
     }
 
-    private void updateEntryWidth(int usableWidth) {
-        System.out.println("!!!" + usableWidth);
+    private void updateNumColumnsAndEntryWidth(int usableWidth) {
+        this.numColumns = usableWidth / MIN_ENTRY_WIDTH;
+        this.numColumns = (this.numColumns <= 0) ? 1 : this.numColumns;
+        assert this.numColumns > 0 : "Number of columns cannot be less than 1!";
+
+        int extraWidth = usableWidth % MIN_ENTRY_WIDTH / this.numColumns;
+        this.entryWidth = MIN_ENTRY_WIDTH + extraWidth;
+        assert this.entryWidth >= MIN_ENTRY_WIDTH : "Calculated entry width is less than the minimum!";
     }
 
     private void updateDisplay() {
+        this.personGridPane.getChildren().clear();
+
+        int row = 0;
+        int col = 0;
+        int id = 1;
+
+        for (Person p : this.personList) {
+            PersonCard pc = new PersonCard(p, id++);
+            String colour = (row % 2 == 0) ? ODD_ROW_COLOUR : EVEN_ROW_COLOUR;
+
+            pc.getRoot().setStyle(colour);
+            pc.getRoot().setPrefWidth(entryWidth);
+            pc.getRoot().setMinHeight(ENTRY_HEIGHT);
+            pc.getRoot().setPrefHeight(ENTRY_HEIGHT);
+            pc.getRoot().setMaxHeight(ENTRY_HEIGHT);
+
+            this.personGridPane.add(pc.getRoot(), col++, row);
+            if (col == numColumns) {
+                col = 0;
+                row++;
+            }
+        }
+
         this.scrollPane.setVvalue(0); // reset scroll to top
     }
 
@@ -67,7 +98,7 @@ public class PersonListPanel extends UiPart<Region> {
 
         this.scrollPane.viewportBoundsProperty().addListener((obs, oldBounds, newBounds) -> {
             int scrollPaneWidth = (int) newBounds.getWidth();
-            updateEntryWidth(scrollPaneWidth);
+            updateNumColumnsAndEntryWidth(scrollPaneWidth);
             updateDisplay();
         });
     }
