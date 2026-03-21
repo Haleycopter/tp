57d8afcc99a23f37bdc2e8890424088506015c9c
CH144 <177772377+CH144@users.noreply.github.com>
2026-03-21 06:31:40 +0000

    Revert "Change to CountDownLatch"
    This reverts commit 9d9ccc1ce00ab8d0251dd932f08893fb453292ef.

--- src/test/java/seedu/address/ui/MainWindowTest.java
+++ src/test/java/seedu/address/ui/MainWindowTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.when;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeoutException;
 
 import org.junit.jupiter.api.Test;
@@ -18,7 +17,6 @@ import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.layout.StackPane;
@@ -30,7 +28,6 @@ import seedu.address.model.person.Person;
 // Code written with help from ChatGPT
 @ExtendWith({ApplicationExtension.class, MockitoExtension.class})
 class MainWindowTest {
-
     private static final double DEFAULT_HEIGHT = 600;
     private static final double DEFAULT_WIDTH = 740;
     private static final Path DEFAULT_PATH = Paths.get("data" , "addressbook.json");
@@ -50,21 +47,13 @@ class MainWindowTest {
     }
 
     @Test
-    void constructMainWindow_defaultDimensions_success() throws InterruptedException {
-        try {
-            Platform.startup(() -> {});
-        } catch (IllegalStateException e) {
-
-        }
-        CountDownLatch latch = new CountDownLatch(1);
-        Platform.runLater(() -> {
+    void constructMainWindow_defaultDimensions_success() throws TimeoutException {
+        FxToolkit.setupFixture(() -> {
             assertEquals(DEFAULT_WIDTH, mainWindow.getRoot().getWidth());
             assertEquals(DEFAULT_HEIGHT, mainWindow.getRoot().getHeight());
-            latch.countDown();
         });
-        latch.await();
     }
-/*
+
     @Test
     void execute_fillInnerParts_success(FxRobot robot) throws TimeoutException {
         FxToolkit.setupFixture(() -> {
@@ -102,5 +91,4 @@ class MainWindowTest {
             assertTrue(helpWindow.isFocused());
         });
     }
-*/
 }
